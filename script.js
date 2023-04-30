dd8449ca4dd00af6ed72ce564788670c24c76c9c
Abdelhadi Benslim <82743999+abdelhadi-benslim-19@users.noreply.github.com>
2023-04-30 20:08:15 +0100

    updated

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+const form = document.querySelector('form');
+const submitBtn = document.querySelector('#submit-btn');
+const fnameInput = document.querySelector('#fname');
+const lnameInput = document.querySelector('#lname');
+const subjectInput = document.querySelector('#subject');
+
+submitBtn.addEventListener('click', function(event) {
+  event.preventDefault();
+  
+  const fnameValue = fnameInput.value.trim();
+  const lnameValue = lnameInput.value.trim();
+  const subjectValue = subjectInput.value.trim();
+  
+  if (!fnameValue || !lnameValue || !subjectValue) {
+    alert('Please fill out all the fields.');
+  } else {
+    alert('The contact form is currently under construction.');
+  }
+});
