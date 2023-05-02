8007f31c3ae7d9bc6743cee376cf21fbd820fbd8
Abdelhadi Benslim <82743999+abdelhadi-benslim-19@users.noreply.github.com>
2023-05-02 10:05:59 +0100

    updated

--- script.js
+++ script.js
@@ -1,55 +1,47 @@
 function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  const topnav = document.getElementById("myTopnav");
+  if (topnav.className === "topnav") {
+    topnav.className += " responsive";
   } else {
-    x.className = "topnav";
+    topnav.className = "topnav";
   }
 }
 
 const form = document.querySelector('form');
-const submitBtn = document.querySelector('#submit-btn');
+const submitButton = document.querySelector('#submit-btn');
 const fnameInput = document.querySelector('#fname');
 const lnameInput = document.querySelector('#lname');
 const subjectInput = document.querySelector('#subject');
 
-submitBtn.addEventListener('click', function(event) {
-  event.preventDefault();
-  
+function validateForm() {
   const fnameValue = fnameInput.value.trim();
   const lnameValue = lnameInput.value.trim();
   const subjectValue = subjectInput.value.trim();
   
   if (!fnameValue || !lnameValue || !subjectValue) {
     alert('Please fill out all the fields.');
-  } else {
+    return false;
+  }
+  
+  return true;
+}
+
+submitButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  
+  if (validateForm()) {
     form.submit();
   }
 });
 
-const popupMessage = document.getElementById('popup-message');
-const closeButton = document.getElementById('close-button');
+const popupContainer = document.querySelector('.popup-container');
+const popupMessage = document.querySelector('.popup-message');
+const closeBtn = document.querySelector('.close-btn');
 
-if (!getCookie('hasSeenMessage')) {
-  popupMessage.style.display = 'block';
+closeBtn.addEventListener('click', () => {
+  popupContainer.computedStyleMap.display = 'none';
+});
 
-  closeButton.addEventListener('click', () => {
-    popupMessage.style.display = 'none';
-    setCookie('hasSeenMessage', true, 7);
-  });
-}
-
-function getCookie(name) {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop().split(';').shift();
-}
-
-function setCookie(name, value, days) {
-  const date = new Date();
-  date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-  const expires = `expires=${date.toUTCString()}`;
-  document.cookie = `${name}=${value}; ${expires}; path=/`;
-}
-
-
+window.addEventListener('load', () => {
+  popupContainer.style.display = 'flex';
+});
